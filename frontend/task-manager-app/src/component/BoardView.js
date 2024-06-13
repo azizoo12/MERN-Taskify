351f8e55a42be507587373a2902f202933fe99d7
nidalmejai <168409932+nidalmejai@users.noreply.github.com>
2024-06-14 01:02:30 +0200

    Update BoardView.js

--- frontend/task-manager-app/src/component/BoardView.js
+++ frontend/task-manager-app/src/component/BoardView.js
@@ -10,7 +10,7 @@ export default function BoardViewPage(){
     const [updateTasks , setupdateTasks] = useState(false);
     const [uncompletedTasks,setuncompletedTasks] = useState();
     const getTasks = async ()=>{
-        const {data} = await axios.get("http://localhost:3002/api/tasks");
+        const {data} = await axios.get("https://mern-taskify-backend.onrender.com/api/tasks");
         if(data.success === true){
             const uncompleted =  
             data.data.filter(e=>e.completed === false)
